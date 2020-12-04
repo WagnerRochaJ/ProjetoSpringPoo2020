dc5e3e11c7b6a615ac162ed769161d82f9df16db
wagnerrocha <jnwagner37@gmail.com>
2020-12-04 16:51:48 -0300

    Testando primeira classe de dominior - categoria

--- src/main/java/com/example/demo/resources/CategoriaResource.java
+++ src/main/java/com/example/demo/resources/CategoriaResource.java
@@ -1,14 +1,25 @@
 package com.example.demo.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
+	
 	@GetMapping
-	public String test() {
-		return "REST funcionando";
+	public List<Categoria> test() {
+		
+		List<Categoria> Categorias = new ArrayList<>();
+		Categoria cat1 = new Categoria("Escritorio");
+		Categoria cat2= new Categoria("Informatica");
+		Categorias.add(cat1);
+		Categorias.add(cat2);
+		return Categorias;
 	}
 }
