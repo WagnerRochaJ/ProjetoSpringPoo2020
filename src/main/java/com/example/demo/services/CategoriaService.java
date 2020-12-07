163dfed2b450be29b2ea2119a4a75b54d40bb646
wagnerrocha <jnwagner37@gmail.com>
2020-12-06 23:10:19 -0300

    Correção do CategoriaService + processo de instancia e teste do endpoint /categorias/{id}

--- src/main/java/com/example/demo/services/CategoriaService.java
+++ src/main/java/com/example/demo/services/CategoriaService.java
@@ -1,18 +1,22 @@
 package com.example.demo.services;
 
+
+
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.Categoria;
 import com.example.demo.repositories.CategoriaRepository;
 
+@Service
 public class CategoriaService {
-
-	private CategoriaRepository repository;
+	@Autowired
+	private CategoriaRepository repositorio;
 	
-	public  Categoria find(Integer id) {
-		
-		Optional<Categoria> obj = repository.findById(id);
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repositorio.findById(id);
 		return obj.orElse(null);
-		
 	}
 }
