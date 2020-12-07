936fdc8fa9f05ca07fcfab779201ae48fee5eb47
wagnerrocha <jnwagner37@gmail.com>
2020-12-07 01:33:04 -0300

    produto associação muitos para muitos

--- src/main/java/com/example/demo/ProjetoSpringPooApplication.java
+++ src/main/java/com/example/demo/ProjetoSpringPooApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.demo.domain.Categoria;
+import com.example.demo.domain.Produto;
 import com.example.demo.repositories.CategoriaRepository;
+import com.example.demo.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ProjetoSpringPooApplication implements CommandLineRunner {
@@ -16,7 +18,10 @@ public class ProjetoSpringPooApplication implements CommandLineRunner {
 		@Autowired
 		private CategoriaRepository categoriaRepository;
 		
-	public static void main(String[] args) {
+		@Autowired
+		private ProdutoRepository produtoRepository;
+		
+		public static void main(String[] args) {
 		SpringApplication.run(ProjetoSpringPooApplication.class, args);
 	}
 
@@ -25,8 +30,21 @@ public class ProjetoSpringPooApplication implements CommandLineRunner {
 
 		Categoria cat1 = new Categoria("informática");
 		Categoria cat2 = new Categoria("Escritório");
-		
+		Produto p1 = new Produto("Computador", 2000.0);
+		Produto p2 = new Produto("Impressora", 800.0);
+  		Produto p3 = new Produto("Mouse", 80.0);
+
+  		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+  		cat2.getProdutos().addAll(Arrays.asList(p2));
+  		
+  		p1.getCategorias().addAll(Arrays.asList(cat1));
+  		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+  		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+
+
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
--- src/main/java/com/example/demo/domain/Categoria.java
+++ src/main/java/com/example/demo/domain/Categoria.java
@@ -1,8 +1,12 @@
 package com.example.demo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+
 
 import com.example.demo.model.EntidadeNomeada;
 
@@ -10,11 +14,20 @@ import com.example.demo.model.EntidadeNomeada;
 public class Categoria extends EntidadeNomeada implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	public Categoria() {
 		
 	}
 	public Categoria(String nome) {
 		this.setNome(nome);
 	}
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
 }
